df72bcdf4ce15b74918b708ef7bda53d43e99630
betir <betirov95@gmail.com>
2021-06-07 00:41:58 +0300

    111

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Usb34',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='betir',
+    author_email='',
+    description=''
+)
